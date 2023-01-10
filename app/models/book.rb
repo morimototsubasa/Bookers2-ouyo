0e9bc4e604821ac8f3470c5f6fbea4762ca376fa
morimototsubasa <119485681+morimototsubasa@users.noreply.github.com>
2023-01-10 08:15:48 +0000

    9a

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+  has_many :view_counts, dependent: :destroy
   
   def self.search_for(content, method)
     if method == 'perfect'
